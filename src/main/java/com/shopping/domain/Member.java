fc47cf6559c17a45dcdeefdc5881e1c3418939a2
zzun0319 <zzun0319@naver.com>
2021-10-08 18:31:40 +0900

    멤버 생성 메서드 따로 추가.

--- src/main/java/com/shopping/domain/Member.java
+++ src/main/java/com/shopping/domain/Member.java
@@ -1,11 +1,14 @@
 package com.shopping.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id @GeneratedValue
@@ -18,4 +21,16 @@ public class Member {
 
     @Enumerated(EnumType.STRING)
     private Grade grade; // 회원 등급
+
+    private Member(String name, String loginId, String password) {
+        this.name = name;
+        this.loginId = loginId;
+        this.password = password;
+        this.grade = Grade.USER;
+    }
+
+    public static Member createMember(String name, String loginId, String password) {
+        return new Member(name, loginId, password);
+    }
+
 }
